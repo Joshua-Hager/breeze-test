6128b91d92f8238337cd2b76db060161bd98608b
Joshua Hager <joshua.hager1990@gmail.com>
2020-08-01 23:30:43 -0700

    parse file

--- src/UploadFile.js
+++ src/UploadFile.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
-import { render } from 'enzyme';
+import Papa from 'papaparse';
 
 class UploadFile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userFile: null
+            userFile: null,
+            fileData: null
         };
         this.onChange = this.onChange.bind(this)
     }
 
+    finishedParsing(results) {
+        console.log(results)
+    }
+
+    componentDidUpdate() {
+        Papa.parse(this.state.userFile, {
+                complete: (results) => this.finishedParsing(results)
+            });
+    }
     onChange(e) {
         this.setState({userFile: e.target.files[0]})
     }
